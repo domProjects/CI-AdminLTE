40bec65a39655bbae6c99bc4d322e9c356171a81
Emmanuel Campait <contact@domprojects.com>
2015-08-31 17:23:26 +0200

    Modification input type Html5

--- application/controllers/admin/Users.php
+++ application/controllers/admin/Users.php
@@ -94,7 +94,7 @@ class Users extends Admin_Controller {
 			$this->data['email'] = array(
 				'name'  => 'email',
 				'id'    => 'email',
-				'type'  => 'text',
+				'type'  => 'email',
                 'class' => 'form-control',
 				'value' => $this->form_validation->set_value('email'),
 			);
@@ -108,7 +108,8 @@ class Users extends Admin_Controller {
 			$this->data['phone'] = array(
 				'name'  => 'phone',
 				'id'    => 'phone',
-				'type'  => 'text',
+				'type'  => 'tel',
+                'pattern' => '^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$',
                 'class' => 'form-control',
 				'value' => $this->form_validation->set_value('phone'),
 			);
@@ -270,7 +271,8 @@ class Users extends Admin_Controller {
 		$this->data['phone'] = array(
 			'name'  => 'phone',
 			'id'    => 'phone',
-			'type'  => 'text',
+            'type'  => 'tel',
+            'pattern' => '^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$',
             'class' => 'form-control',
 			'value' => $this->form_validation->set_value('phone', $user->phone)
 		);
